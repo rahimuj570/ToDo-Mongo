361e251bae505af6b23be815064b7c903014f8ae
rahimuj570 <rahimuj570@gmail.com>
2022-05-18 23:20:51 +0600

    successfully get and post data

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,14 +1,47 @@
-import React from "react";
+import userEvent from "@testing-library/user-event";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { FaRegTrashAlt, FaClipboardCheck } from "react-icons/fa";
+import { toast } from "react-toastify";
+import auth from "../firebase.int";
 
 const Todo = () => {
+  const [tasks, setTasks] = useState();
+  const [user] = useAuthState(auth);
+  const addTask = (e) => {};
+  const [refetch, setReFetch] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/tasks")
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  }, [refetch]);
+
   return (
     <>
       <form
         className="mt-10"
         onSubmit={(e) => {
           e.preventDefault();
-          const data = { name: e.target.name.value, task: e.target.task.value };
+          const data = {
+            name: e.target.name.value,
+            task: e.target.task.value,
+            uid: user.uid,
+          };
+          fetch("http://localhost:5000/add", {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(data),
+          })
+            .then((res) => res.json())
+            .then((result) => {
+              console.log(result);
+            });
+          e.target.reset();
+          toast.success("Task Added");
+          setReFetch(!refetch);
         }}
       >
         <div className="text-center">
@@ -51,23 +84,25 @@ const Todo = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
-            <td className="px-6 py-4">{"supplier"}</td>
-            <td className="px-6 py-4">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
-              itaque aut qui et aliquid non odio enim nostrum quibusdam omnis?
-            </td>
-            <td className="px-6 py-4 text-right">
-              <div>
-                <FaClipboardCheck className="text-sky-600 cursor-pointer hover:text-sky-500 text-2xl" />
-              </div>
-            </td>
-            <td className="px-6 py-4 text-right">
-              <div>
-                <FaRegTrashAlt className="text-red-600 cursor-pointer hover:text-red-500 text-2xl" />
-              </div>
-            </td>
-          </tr>
+          {tasks.map((task) => (
+            <tr
+              key={task?._id}
+              className="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700"
+            >
+              <td className="px-6 py-4">{task?.name}</td>
+              <td className="px-6 py-4">{task?.task}</td>
+              <td className="px-6 py-4 text-right">
+                <div>
+                  <FaClipboardCheck className="text-sky-600 cursor-pointer hover:text-sky-500 text-2xl" />
+                </div>
+              </td>
+              <td className="px-6 py-4 text-right">
+                <div>
+                  <FaRegTrashAlt className="text-red-600 cursor-pointer hover:text-red-500 text-2xl" />
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
