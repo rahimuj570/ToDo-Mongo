29af96ac2ff057d2c904925705f97e7f090c5508
rahimuj570 <rahimuj570@gmail.com>
2022-05-18 21:42:40 +0600

    socil signin added

--- src/Components/ManageUser/SocialSignin.js
+++ src/Components/ManageUser/SocialSignin.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub, FaFacebook } from "react-icons/fa";
+import auth from "../../firebase.int";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import Loading from "../Loading";
+import { async } from "@firebase/util";
 
 const SocialSignin = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const [signInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+  if (googleLoading) {
+    return <Loading />;
+  }
+  return (
+    <>
+      <div className="my-8 mx-2 items-center flex justify-center">
+        <div className="rounded h-1 sm:w-2/6 md:w-1/6 w-full bg-sky-200"></div>
+        <p className="font-semibold mx-2">OR</p>
+        <div className="rounded h-1 sm:w-2/6 md:w-1/6 w-full bg-sky-200"></div>
+      </div>
+
+      <div className="sm:w-2/6 w-5/6 mx-auto mb-10">
+        <div
+          onClick={async () => {
+            await signInWithGoogle();
+            navigate("/");
+          }}
+          className="mb-3 cursor-pointer duration-300  hover:bg-slate-50 rounded-md   py-2 text-xl flex items-center justify-center px-5 shadow"
+        >
+          <FcGoogle className="basis-10 text-3xl mx-1" />{" "}
+          <p className="">Continue With Google</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SocialSignin;
