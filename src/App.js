b0cd00fde4ebfa9720f891cb83db72a5da162c68
rahimuj570 <rahimuj570@gmail.com>
2022-05-18 21:15:02 +0600

    footer is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
